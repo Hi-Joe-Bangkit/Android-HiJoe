44b3e55dce3eb220f67e7f6dca790d9359a8de60
Timur Haryo <timurharyo00@gmail.com>
2021-05-22 18:52:38 +0700

    init project

---
+++ app/src/main/java/id/capstone/hijoe/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package id.capstone.hijoe.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.capstone.hijoe.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
