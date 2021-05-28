c96226a5176f68d53e8c3ceec12b2f5d215d74e6
Timur Haryo <timurharyo00@gmail.com>
2021-05-28 07:29:06 +0700

    create open gallery function

--- app/src/main/java/id/capstone/hijoe/ui/main/MainActivity.kt
+++ app/src/main/java/id/capstone/hijoe/ui/main/MainActivity.kt
@@ -1,22 +1,70 @@
 package id.capstone.hijoe.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
+import android.provider.MediaStore
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import id.capstone.hijoe.R
 import id.capstone.hijoe.databinding.ActivityMainBinding
+import id.capstone.hijoe.util.toast
 
 class MainActivity : AppCompatActivity() {
-    private var binding: ActivityMainBinding? = null
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding!!.root)
+        setContentView(binding.root)
+
         setupView()
     }
 
     private fun setupView() {
-        binding!!.btnOpenCamera.setOnClickListener { v: View? -> Toast.makeText(this, "open camera", Toast.LENGTH_SHORT).show() }
-        binding!!.btnOpenGallery.setOnClickListener { v: View? -> Toast.makeText(this, "open gallery", Toast.LENGTH_SHORT).show() }
+        with(binding) {
+            btnOpenCamera.setOnClickListener {
+                toast("open camera")
+            }
+            btnOpenGallery.setOnClickListener {
+                toast("open gallery")
+            }
+        }
+    }
+
+    private fun openGallery() {
+        val getIntent = Intent(Intent.ACTION_GET_CONTENT)
+        getIntent.type = "image/*"
+
+        val pickIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        pickIntent.type = "image/*"
+
+        val chooserIntent = Intent.createChooser(getIntent, getString(R.string.select_image))
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayListOf(pickIntent))
+
+        startActivityForResult(chooserIntent, GALLERY_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK) {
+            when(resultCode) {
+                CAMERA_REQUEST_CODE -> {
+                    // todo: get camera bitmap
+                }
+                GALLERY_REQUEST_CODE -> {
+                    // todo: get gallery data
+                }
+                else -> {
+                    Log.e(this.javaClass.simpleName, "Illegal request code")
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val CAMERA_REQUEST_CODE = 1000
+        private const val GALLERY_REQUEST_CODE = 1001
     }
 }
